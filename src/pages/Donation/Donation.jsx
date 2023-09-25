cff14a2ccbee1de5fec27f885eb3f04d3aa0a69d
rafikulislam775 <islamrafikul.jp@gmail.com>
2023-09-26 08:37:19 +0900

    added see more and see less btn

--- src/pages/Donation/Donation.jsx
+++ src/pages/Donation/Donation.jsx
@@ -4,7 +4,8 @@ import DonationCard from "./DonationCard";
 const Donation = () => {
   const [donated, setDonated] = useState([]);
   const [noFound, setNoFound] = useState(false);
-  
+  const [isShow, setIsShow] = useState(false);
+
   useEffect(() => {
     const donatedItems = JSON.parse(localStorage.getItem("donation"));
     if (!donatedItems) {
@@ -25,10 +26,28 @@ const Donation = () => {
             </h1>
           </div>
         ) : (
-          <div className="container m-auto mt-10 grid grid-cols-1 md:grid-cols-2 gap-5">
-            {donated.map((card, index) => (
-              <DonationCard key={index} card={card}></DonationCard>
-            ))}
+          <div>
+            <div className="container m-auto mt-10 grid grid-cols-1 md:grid-cols-2 gap-5">
+              {isShow
+                ? donated.map((card, index) => (
+                    <DonationCard key={index} card={card}></DonationCard>
+                  ))
+                : donated
+                    .slice(0, 4)
+                    .map((card, index) => (
+                      <DonationCard key={index} card={card}></DonationCard>
+                    ))}
+            </div>
+            {donated.length > 4 && (
+              <div className="flex justify-center my-10">
+                <button
+                  onClick={() => setIsShow(!isShow)}
+                  className="btn px-10 bg-[#009444] text-white "
+                >
+                  {isShow ? "See Less" : "See More"}
+                </button>
+              </div>
+            )}
           </div>
         )}
       </div>
