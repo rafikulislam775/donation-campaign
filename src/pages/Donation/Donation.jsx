3b385061ad5b2ae88873d2e6cb9474d1e7e6f9e0
rafikulislam775 <islamrafikul.jp@gmail.com>
2023-09-25 22:25:59 +0900

    load  data from local storage and set donation page with responsive

--- src/pages/Donation/Donation.jsx
+++ src/pages/Donation/Donation.jsx
@@ -1,7 +1,26 @@
+import { useEffect, useState } from "react";
+import DonationCard from "./DonationCard";
+
 const Donation = () => {
+  const [donated, setDonated] = useState([]);
+  const [noFound, setNoFound] = useState(false);
+  useEffect(() => {
+    const donatedItems = JSON.parse(localStorage.getItem("donation"));
+    if (donated) {
+      setDonated(donatedItems);
+    } else {
+      setNoFound("no donation found");
+    }
+  }, []);
+  console.log(donated, noFound);
+
   return (
     <div>
-      <h2>form donation......</h2>
+      <div className="container m-auto mt-10 grid grid-cols-1 md:grid-cols-2 gap-5">
+        {donated.map((card, index) => (
+          <DonationCard key={index} card={card}></DonationCard>
+        ))}
+      </div>
     </div>
   );
 };
---
+++ src/pages/Donation/DonationCard.jsx
@@ -0,0 +1,57 @@
+const DonationCard = ({ card }) => {
+  const {
+    id,
+    img,
+    title,
+    price,
+    category,
+    category_bg_color,
+    card_bg_color,
+    text_color,
+  } = card;
+  console.log(card);
+  const cardBg = {
+    backgroundColor: card_bg_color,
+  };
+  const titleBg = {
+    backgroundColor: category_bg_color,
+    color: text_color,
+  };
+
+  const textColor = {
+    color: text_color,
+  };
+  return (
+    <div>
+      <div
+        className="relative flex flex-row rounded-xl shadow-md"
+        style={cardBg}
+      >
+        <div className="relative m-0 w-2/5 shrink-0 overflow-hidden rounded-xl rounded-r-none bg-white bg-clip-border text-gray-700">
+          <img src={img} alt="image" className="h-full w-full object-cover" />
+        </div>
+        <div className="p-6">
+          <span className=" rounded p-1 px-2 font-bold" style={titleBg}>
+            {category}
+          </span>
+          <h4 className="my-2 block font-sans text-2xl font-semibold h-12 ">
+            {title}
+          </h4>
+          <p
+            className="mb-8 block font-sans text-base font-extrabold leading-relaxed antialiased"
+            style={textColor}
+          >
+            $ {price}
+          </p>
+          <a className="inline-block" href="#">
+            <button className="btn bg-red-500 text-white" type="button">
+              View Details
+            </button>
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default DonationCard;
