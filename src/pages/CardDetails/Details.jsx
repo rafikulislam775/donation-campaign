80e43a46ec8e03e7d87c404eb568952bf2f56a56
rafikulislam775 <islamrafikul.jp@gmail.com>
2023-09-25 21:17:38 +0900

    added function to add donated items in local storage

--- src/pages/CardDetails/Details.jsx
+++ src/pages/CardDetails/Details.jsx
@@ -1,5 +1,26 @@
 const Details = ({ card }) => {
-  const { img, description, title, price } = card;
+  const { id, img, description, title, price } = card;
+
+  const handleDonation = () => {
+    const addToDonation = [];
+    const donatedItems = JSON.parse(localStorage.getItem("donation"));
+    if (!donatedItems) {
+      addToDonation.push(card);
+      localStorage.setItem("donation", JSON.stringify(addToDonation));
+    } else {
+      const isExist = donatedItems.find((data) => data.id === id);
+      if (!isExist) {
+        addToDonation.push(...donatedItems, card);
+        localStorage.setItem("donation", JSON.stringify(addToDonation));
+        alert("added");
+      } else {
+        alert("already added");
+      }
+    }
+    console.log(addToDonation);
+  };
+  console.log(card);
+
   return (
     <div>
       <div className=" mt-10 container mx-auto flex justify-center items-center   ">
@@ -7,15 +28,18 @@ const Details = ({ card }) => {
           <div className="relative">
             <img className="w-full" src={img} alt="" />
             <div className="absolute bottom-0 left-0 right-0 p-5 bg-black bg-opacity-70">
-              <button className="btn text-white bg-red-600">
+              <button
+                onClick={handleDonation}
+                className="btn text-white bg-red-600"
+              >
                 Donate $ {price}
               </button>
             </div>
           </div>
 
           <div className="card-body">
-            <h2 className="card-title">{title}</h2>
-            <p>{description}</p>
+            <h2 className="card-title font-bold text-2xl  ">{title}</h2>
+            <p className=" text-lg">{description}</p>
           </div>
         </div>
       </div>
