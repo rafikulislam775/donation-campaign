62b1a61ff1bdb4aa10aa8bf09fe0c7d5ba58a233
rafikulislam775 <islamrafikul.jp@gmail.com>
2023-09-27 19:59:52 +0900

    added no donation found page in Statistics pages

--- src/pages/Statistics/Statistics.jsx
+++ src/pages/Statistics/Statistics.jsx
@@ -6,6 +6,7 @@ const Statistics = () => {
   const allData = useLoaderData();
   const [totalPrice, setTotalPrice] = useState(0);
   const [price, setPrice] = useState(0);
+  const [noFound, setNoFound] = useState(false);
 
   useEffect(() => {
     const totalPrices = allData.reduce(
@@ -14,53 +15,68 @@ const Statistics = () => {
     );
     setTotalPrice(totalPrices);
     const getData = JSON.parse(localStorage.getItem("donation"));
-    const totalDonation = getData.reduce(
-      (preValue, curValue) => preValue + parseFloat(curValue.price),
-      0
-    );
-    setPrice(totalDonation);
+    if (getData) {
+      const totalDonation = getData.reduce(
+        (preValue, curValue) => preValue + parseFloat(curValue.price),
+        0
+      );
+      setPrice(totalDonation);
+    } else {
+      setNoFound(true);
+    }
   }, [allData]);
+  console.log(noFound);
 
-  const number = 55;
   const data = [
-    ["Task", "Hours per Day"],
-    ["Work", totalPrice],
+    ["Task", "Donation pie chart"],
+    ["totalPrice", totalPrice],
     // ["Eat", 22, "color: #ff0000"],
-    ["Platinum", price],
+    ["donation price", price],
   ];
   const options = {
-    title: "My Daily Activities",
+    // title: "My Daily Activities",
     // legend for position change
     // legend: { position: "bottom" },
     legend: "none", // Hide the default legend
 
     // index k dore color change kora
     slices: {
-      0: { color: "green", maxLines: 3 },
-      1: { color: "pink" },
+      0: { color: "Red" },
+      1: { color: "#00C49F" },
     },
   };
 
   return (
-    <div className="">
-      <Chart
-        chartType="PieChart"
-        data={data}
-        options={options}
-        width={"100%"}
-        height={"400px"}
-      />
-      <div className="flex justify-center gap-5">
-        <div className="flex items-center  gap-2">
-          <div className="w-16 h-3 bg-green-500 mt-1"></div>
-          work
+    <div>
+      {noFound ? (
+        <div>
+          <h1 className="flex justify-center items-center h-[70vh] mb-5 text-5xl font-bold opacity-30">
+            No Donation Found !!
+          </h1>
         </div>
-        <div className="flex items-center  gap-2">
-          <div className="w-16 h-3 bg-pink-200 mt-1"></div>
-          Platinum
+      ) : (
+        <div className="">
+          <Chart
+            chartType="PieChart"
+            data={data}
+            options={options}
+            width={"100%"}
+            height={"400px"}
+          />
+          <div className="flex justify-center gap-5">
+            <div className="flex items-center  gap-2">
+              Your Donation
+              <div className="w-16 h-3 bg-[#00C49F] mt-1"></div>
+            </div>
+            <div className="flex items-center  gap-2">
+              {" "}
+              Total Donation
+              <div className="w-16 h-3 bg-red-500 mt-1"></div>
+            </div>
+            <div></div>
+          </div>
         </div>
-        <div></div>
-      </div>
+      )}
     </div>
   );
 };
